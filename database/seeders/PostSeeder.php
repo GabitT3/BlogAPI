f13d967a54fa1c70387c96806de5be2e636f92b9
GabitT3 <gabitandjong9@gmail.com>
2024-12-01 21:30:31 +0100

    Add API documentation to README and update models and seeders

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::first();
+
+    if (!$user) {
+        $user = User::factory()->create(); 
+    }
+
+    Post::factory()->count(10)->create([
+        'user_id' => $user->id, 
+    ]);
+        
+        
+    }
+}
